dbe280cceaba6a9ce5692e694f941b635a5ca83b
johns789 <tayj10@gmail.com>
2014-12-16 16:26:31 -0500

    sema_list implementation

--- sema_list.c
+++ sema_list.c
@@ -10,15 +10,16 @@ struct List {
 struct List * head = NULL;
 
 // More variables
-
+pthread_mutex_t mutexLock;
+sema_t fullSem;
+sema_t emptySem;
 
 main()
 {
 
- sema_t sem;
-
-
-
+ pthread_mutex_init(&mutexLock, NULL);
+ sema_init(&fullSem, 20, USYNC_THREAD, NULL);
+ sema_init(&emptySem, 0, USYNC_THREAD, NULL);
 
 
 }
@@ -26,31 +27,26 @@ main()
 void insert( int val )
 {
 
-
+  sema_wait(&fullSem);
   List tmp = new List;
-
-
   tmp->val = val;
-
-
+  pthread_mutex_lock(&mutexLock);
   tmp->next = head;
-
-
   head = tmp;
-
+  ptherad_mutex_unlock(&mutexLock);
+  sema_post(&emptySem);
 
 }
 
 Struct List * removeFirst()
 {
 
-
+  sema_wait(&emptySem);
+  pthread_mutex_lock()
   List tmp = head;
-
-
-
   head = tmp->next;
-
+  sema_post(&fullSem);
+  ptherad_mutex_unlock(&mutexLock);
 
   return tmp;
 
